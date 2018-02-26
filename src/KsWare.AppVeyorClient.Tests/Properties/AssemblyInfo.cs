653613d524891880294eff0d35a18e3c02cd3a42
SchreinerK <ks@ksware.de>
2018-02-26 20:29:36 +0100

    AssemblyVersion

--- src/KsWare.AppVeyorClient.Tests/Properties/AssemblyInfo.cs
+++ src/KsWare.AppVeyorClient.Tests/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("KsWare.AppVeyorClient.Tests")]
+[assembly: AssemblyTitle("KsWare AppVeyorClient Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KsWare.AppVeyorClient.Tests")]
+[assembly: AssemblyCompany("KsWare")]
+[assembly: AssemblyProduct("AppVeyorClient")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("92b3f74d-53e7-4d69-b54a-083ca80de6e5")]
 
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
