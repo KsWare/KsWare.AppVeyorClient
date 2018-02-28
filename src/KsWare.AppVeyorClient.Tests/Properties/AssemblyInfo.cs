ebad50d68794eabe3bd7eaa6aacb46298437ecd4
SchreinerK <ks@ksware.de>
2018-02-28 11:45:56 -0800

    [skip ci] increment version $Env:APPVEYOR_BUILD_VERSION

--- src/KsWare.AppVeyorClient.Tests/Properties/AssemblyInfo.cs
+++ src/KsWare.AppVeyorClient.Tests/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("92b3f74d-53e7-4d69-b54a-083ca80de6e5")]
 
-[assembly: AssemblyVersion("0.1.10")]
-[assembly: AssemblyFileVersion("0.1.10")]
-[assembly: AssemblyInformationalVersion("0.1.10+20180228112526")]
+[assembly: AssemblyVersion("0.1.11")]
+[assembly: AssemblyFileVersion("0.1.11")]
+[assembly: AssemblyInformationalVersion("0.1.11+20180228114525")]
