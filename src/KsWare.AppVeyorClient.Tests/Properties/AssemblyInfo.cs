326ed77bdf36f5577004eb7e98b3f70c81805e58
SchreinerK <ks@ksware.de>
2018-02-28 11:56:19 -0800

    [skip ci] increment version 0.1.12+20180228115549

--- src/KsWare.AppVeyorClient.Tests/Properties/AssemblyInfo.cs
+++ src/KsWare.AppVeyorClient.Tests/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("92b3f74d-53e7-4d69-b54a-083ca80de6e5")]
 
-[assembly: AssemblyVersion("0.1.11")]
-[assembly: AssemblyFileVersion("0.1.11")]
-[assembly: AssemblyInformationalVersion("0.1.11+20180228114525")]
+[assembly: AssemblyVersion("0.1.12")]
+[assembly: AssemblyFileVersion("0.1.12")]
+[assembly: AssemblyInformationalVersion("0.1.12+20180228115549")]
