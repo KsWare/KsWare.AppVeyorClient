43b67e51cc328e65a7480f1d38662e9adc263df0
Appveyor-KsWare <appveyor@ksware.de>
2018-03-11 12:39:27 -0700

    [skip ci] increment version 0.2.3+master+20180311123849

--- src/KsWare.AppVeyorClient/Properties/AssemblyInfo.cs
+++ src/KsWare.AppVeyorClient/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Windows;
 											  // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("0.2.2")]
-[assembly: AssemblyFileVersion("0.2.2")]
-[assembly: AssemblyInformationalVersion("0.2.2+$(build_meta)")]
+[assembly: AssemblyVersion("0.2.3")]
+[assembly: AssemblyFileVersion("0.2.3")]
+[assembly: AssemblyInformationalVersion("0.2.3+$(build_meta)")]
