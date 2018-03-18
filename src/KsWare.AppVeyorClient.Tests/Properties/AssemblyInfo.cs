25418f2466aea05e6672dd7032637d5be8aa61ce
%github_user_name_appveyor% <%github_user_email_appveyor%>
2018-03-18 15:16:05 -0700

    [skip ci] increment version 0.2.24+20180318151518

--- src/KsWare.AppVeyorClient.Tests/Properties/AssemblyInfo.cs
+++ src/KsWare.AppVeyorClient.Tests/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("92b3f74d-53e7-4d69-b54a-083ca80de6e5")]
 
-[assembly: AssemblyVersion("0.2.23")]
-[assembly: AssemblyFileVersion("0.2.23")]
-[assembly: AssemblyInformationalVersion("0.2.23+20180318150547")]
+[assembly: AssemblyVersion("0.2.24")]
+[assembly: AssemblyFileVersion("0.2.24")]
+[assembly: AssemblyInformationalVersion("0.2.24+20180318151518")]
