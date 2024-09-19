f50c61a617581edb7d5ca2503242015d421adc42
SchreinerK <SchreinerK@users.noreply.github.com>
2024-09-19 23:44:49 +0200

    .

--- src/build/build.cmd
+++ src/build/build.cmd
@@ -3,7 +3,9 @@ setlocal
 set Configuration=Release
 ::set ApplicationVersion=0.2.46.0
 set path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin
-msbuild "..\KsWare.AppVeyorClient\KsWare.AppVeyorClient.csproj" /verbosity:normal /target:publish
+nuget update -self
+nuget restore ..\..\build\ci.sln
+::msbuild "..\KsWare.AppVeyorClient\KsWare.AppVeyorClient.csproj" /verbosity:normal /target:publish
 ::msdeploy.exe -verb:sync -source:dirPath="..\bin\Release" -dest:dirPath="..\deploy" -enableRule:DoNotDeleteRule
 pause
 endlocal
